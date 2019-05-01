7ee81919f7e2619d65c5edb01aba9b93fc0320b2
DENNIS KIPCHUMBA <46906968+arapchirchir@users.noreply.github.com>
2019-05-01 10:37:59 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,58 @@
+//sticky navigation on scroll..
+const nav = document.querySelector('#navigation');
+const navTop = nav.offsetTop;
+
+function stickyNavigation() {
+  console.log('navTop = ' + navTop);
+  console.log('scrollY = ' + window.scrollY);
+
+  if (window.scrollY >= navTop) {
+    // nav offsetHeight = height of nav
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove('fixed-nav');
+  }
+}
+
+window.addEventListener('scroll', stickyNavigation);
+
+//alternating images scripts
+
+//alternating images scripts
+window.addEventListener("DOMContentLoaded", function(e) {
+
+  var stage = document.getElementById("stage");
+  var fadeComplete = function(e) { stage.appendChild(arr[0]); };
+  var arr = stage.getElementsByClassName("slides");
+  for(var i=0; i < arr.length; i++) {
+    arr[i].addEventListener("animationend", fadeComplete, false);
+  }
+
+}, false);
+
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+var i;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";  
+}
+slideIndex++;
+if (slideIndex > slides.length) {slideIndex = 1}    
+for (i = 0; i < dots.length; i++) {
+  dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";  
+dots[slideIndex-1].className += " active";
+setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+
+
